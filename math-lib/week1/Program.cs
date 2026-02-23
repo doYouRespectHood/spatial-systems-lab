bb3298deb6b2f101dbf0ec3121993570a274ac1e
Will Jacobi <willwardjacobi@gmail.com>
2026-02-23 14:12:04 -0700

    Week 1: Initial matrix-vector transformation experiment

---
+++ math-lib/week1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+        static void Main()
+        {
+            double[,] matrix =
+        {
+            {1, 0, 0},
+            {0, 2, 0},
+            {0, 0, 3}
+        };
+        
+        double[] vector = {1, 1, 1};
+        double[] result = new double[3];
+
+        for (int i = 0; i <3; i++)
+        {
+            result[i] = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                result[i] += matrix[i, j] * vector [j];
+            }
+        }
+
+        Console.WriteLine($"Result: [{result[0]}, {result[1]}, {result[2]}]");
+        }
+}
